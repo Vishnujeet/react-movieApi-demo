3517f1e209553fd2414c8188c7ed5380f221d41a
Visnujeet <vishnujeet.singh@lighting.com>
2018-09-01 22:44:46 +0530

    VS : Movie details added

--- src/Components/movieInDetails.js
+++ src/Components/movieInDetails.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
+var imgStyle = {
 
+  };
+  
 const MoiveIndetails = (props) => {
     return ( 
         <div className='row'>
-            <div className='col s6'>
+            <div className='col s4'>
                 <div className='card'>
                     <div className='card-image'>
-                        <img src={'https://image.tmdb.org/t/p/w500/'+props.movie.backdrop_path} alt={props.movie.title}/>
+                        <img style={imgStyle} src={'https://image.tmdb.org/t/p/w500/'+props.movie.backdrop_path} alt={props.movie.title}/>
                         <span className='card-title'>{props.movie.title}</span>
                     </div>
                     <div className='card-content'>
@@ -21,7 +24,16 @@ const MoiveIndetails = (props) => {
                     </div>
                 </div>
             </div>
-            <div className='col s6'>
+            <div className='col s8 '>
+            <div>
+            <p class="left-align">Movie Title : {props.movie.original_title}</p>
+            <p class="left-align">Move Overview : {props.movie.overview}</p>
+            <p class="left-align">Release Date : {props.movie.release_date}</p>
+            <p class="left-align">Average Vote : {props.movie.vote_average}</p>
+          </div>
+                <div></div> <br/>
+                <div></div> <br/>
+                <div></div> <br/>
             </div>
          </div>
         
