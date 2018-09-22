058be930c6da7b3a5b6dbac5912948f65b052f29
Visnujeet <vishnujeet1989@gmail.com>
2018-09-22 23:37:26 +0530

    VS : stateful structure added

--- src/Components/moviesInDetails.js
+++ src/Components/moviesInDetails.js
@@ -1,44 +1,61 @@
-import React from 'react'
+import React, { Component } from 'react'
 var imgStyle = {
 
   };
   
-const MoivesIndetails = (props) => {
-    return ( 
-        <div className='row'>
-            <div className='col s4'>
-                <a class="left-align" href='/'>Go Back To Home</a>
-                <div className='card'>
-                    <div className='card-image'>
-                        <img style={imgStyle} src={'https://image.tmdb.org/t/p/w500/'+props.movie.backdrop_path} alt={props.movie.title}/>
-                        <span className='card-title'>{props.movie.title}</span>
-                    </div>
-                    <div className='card-content'>
-                    
-                        <p>{props.movie.original_title}</p>
-                    </div>    
-                    <div className='card-action'>
-                        <div>
-                            <span>Vote : {props.movie.vote_average}     </span>
-                            {/* <a href={props.movie.title} target='_blank'>View More... </a> */}
+  class MoivesIndetails extends Component{
+      constructor(props){
+          super(props)
+          this.state={
+            moivesIndetail:[]
+          }        
+      }
+      
+      componentDidMount(){
+          this.setState({
+              moivesIndetail:this.props.movie
+          })
+      }
+      render(){
+        return ( 
+            <div className='row'>
+                <div className='col s4'>
+                    <a class="left-align" href='/'>Go Back To Home</a>
+                    <div className='card'>
+                        <div className='card-image'>
+                            <img style={imgStyle} src={'https://image.tmdb.org/t/p/w500/'+this.props.movie.backdrop_path} alt={this.state.moivesIndetail.title}/>
+                            <span className='card-title'>{this.state.moivesIndetail.title}</span>
+                        </div>
+                        <div className='card-content'>
+                        
+                            <p>{this.state.moivesIndetail.original_title}</p>
+                        </div>    
+                        <div className='card-action'>
+                            <div>
+                                <span>Vote : {this.state.moivesIndetail.vote_average}     </span>
+                                {/* <a href={props.movie.title} target='_blank'>View More... </a> */}
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className='col s8 '>
-            <div>
-            <p class="left-align">Movie Title : {props.movie.original_title}</p>
-            <p class="left-align">Move Overview : {props.movie.overview}</p>
-            <p class="left-align">Release Date : {props.movie.release_date}</p>
-            <p class="left-align">Average Vote : {props.movie.vote_average}</p>
-          </div>
-                <div></div> <br/>
-                <div></div> <br/>
-                <div></div> <br/>
-            </div>
-         </div>
-        
-     );
-}
+                <div className='col s8 '>
+                <div>
+                <p class="left-align">Movie Title : {this.state.moivesIndetail.original_title}</p>
+                <p class="left-align">Move Overview : {this.state.moivesIndetail.overview}</p>
+                <p class="left-align">Release Date : {this.state.moivesIndetail.release_date}</p>
+                <p class="left-align">Average Vote : {this.state.moivesIndetail.vote_average}</p>
+              </div>
+                    <div></div> <br/>
+                    <div></div> <br/>
+                    <div></div> <br/>
+                </div>
+             </div>
+            
+         );
+      }
+  }
+// const MoivesIndetails = (props) => {
+    
+// }
  
 export default MoivesIndetails;
